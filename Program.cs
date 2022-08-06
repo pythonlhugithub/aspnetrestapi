7d8eb654609055bed661dd1d62796c17af815db2
david lizhong huang <davidlizhonghuang@users.noreply.github.com>
2022-08-06 14:18:24 +1000

    add cors

--- Program.cs
+++ Program.cs
@@ -5,18 +5,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-app.UseHttpsRedirection();
+ 
+
+ builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddCors(options =>
+// {
+//     options.AddDefaultPolicy(
+//                       policy  =>
+//                       {
+//                           policy.WithOrigins("http://localhost:4200", "https://localhost:7187")
+//                           .AllowAnyHeader()
+//                             .AllowAnyMethod();
+//                       });
+// });
+
+app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+        app.UseHttpsRedirection(); 
+
+ 
 
 app.UseAuthorization();
 
